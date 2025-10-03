e5764fe04c2be3cf9a9498cba6bf50dc3fbbaf37
arsene-bedoucha <arsene.bedoucha@edu.esiee.fr>
2025-10-03 09:01:12 +0000

    arsene-bedoucha listes.csv

--- main.py
+++ main.py
@@ -1,52 +1,88 @@
-#### Imports et définition des variables globales
+'''Travailler avec les outils Python à partir d'un fichier .csv'''
 
+#### Imports et définition des variables globales ###
+
+import csv
 FILENAME = "listes.csv"
 
-#### Fonctions secondaires
+#### Fonctions secondaires ###
 
 def read_data(filename):
-    """retourne le contenu du fichier <filename>
+    '''Place les éléments du fichier dans une liste des listes'''
 
-    Args:
-        filename (str): nom du fichier à lire
-
-    Returns:
-        list: le contenu du fichier (1 list par ligne)
-    """
     l = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        r = csv.reader(f, delimiter=';')
+        for row in r:
+            liste_entiers = [int(x.strip()) for x in row]
+            l.append(liste_entiers)
     return l
 
 def get_list_k(data, k):
-    l = []
+    '''Prend la kième liste de la liste de listes'''
+
+    if k > len(data) or data == []:
+        return False
+    l = data[k]
     return l
 
 def get_first(l):
+    '''Retroune le premier élément de la liste retenue'''
+
+    i = 0
+    if l != []:
+        return l[i]
     return None
 
 def get_last(l):
+    '''Retroune le dernier élément de la liste retenue'''
+
+    i = len(l)-1
+    if l != []:
+        return l[i]
     return None
 
 def get_max(l):
+    '''Retroune l'élément maximal de la liste retenue'''
+
+    if l != []:
+        return max(l)
     return None
 
 def get_min(l):
+    '''Retroune l'élément minimal de la liste retenue'''
+
+    if l != []:
+        return min(l)
     return None
 
 def get_sum(l):
+    '''Retroune la somme des éléments de la liste retenue'''
+
+    if l != []:
+        x = 0
+        for value in l:
+            x = x + value
+        return x
     return None
 
-
-#### Fonction principale
-
+#### Fonction principale ###
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    '''Fait appel aux fonctions secondaires'''
 
+    data = read_data(FILENAME)
+    print('Numéro et affichage des listes contenues dans le fichier :')
+    for i, l in enumerate(data):
+        print(i, l)
+    k = int(input("Choisir un numéro de liste !"))
+    print('Voici la liste numéro :', k, '-', get_list_k(data, k))
+    l = get_list_k(data, k)
+    print('Premier élément de cette liste :', get_first(l))
+    print('Dernier élément de cette liste :', get_last(l))
+    print('Elément maximum de cette liste :', get_max(l))
+    print('Elément minimum de cette liste :', get_min(l))
+    print('Somme des éléments de cette liste :', get_sum(l))
 
 if __name__ == "__main__":
     main()
